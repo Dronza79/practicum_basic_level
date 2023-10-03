a111be20b681b80eef14d0fb1a86216c5367609e
Зайцев Андрей <mr_zaycev@mail.ru>
2023-10-03 22:29:32 +0300

    создал шаблон модалки

--- js/main.js
+++ js/main.js
@@ -5,75 +5,75 @@
     const sch = document.querySelector('.search_form');
     const addBtn = document.getElementById('add-btn');
     const tBody = document.getElementById('t-body');
-    return {tHead, sch, addBtn, tBody};
+	  const bgM = document.createElement('div');
+	  bgM.classList.add('bgModal');
+	  document.body.append(bgM);
+		return {bgM, tHead, sch, addBtn, tBody};
   }
-
-	function createBgModal() {
-		const bgM = document.createElement('div');
-		bgM.classList.add('bgModal');
-		document.body.append(bgM);
-
+	
+	function removeModalVisible(windowModal, modalBackGround) {
+		windowModal.classList.remove('win-active');
+		modalBackGround.classList.remove('modal-visible');
+		document.body.style.overflow = 'inherit';
+	}
+	
+	function createModalWindowTemplate() {
+		const winTemplate = document.createElement('div');
+		const btnClose = document.createElement('button');
+		const btnMain = document.createElement('button');
+		const btnCancel = document.createElement('button');
+		const title = document.createElement('h3');
+		const img = document.createElement('img');
+		const html = getHTMLElement();
+		
+		img.src = 'img/close.svg';
+		btnCancel.textContent = 'Отмена';
+		title.classList.add('modal-title');
+		btnClose.classList.add('modal-btnClose');
+		btnMain.classList.add('btn');
+		btnCancel.classList.add('modal-btnCancel');
+		
+		btnClose.append(img);
+		winTemplate.append(title, btnClose, btnMain, btnCancel);
+		
+		html.bgM.classList.add('modal-visible');
+		html.bgM.append(winTemplate);
+		winTemplate.classList.add('win-active');
+		document.body.style.overflow = 'hidden';
+		
+		function funRMV() {
+			removeModalVisible(winTemplate, html.bgM);
+		}
+		
+		btnMain.addEventListener('click', funRMV);
+		btnCancel.addEventListener('click', funRMV);
+		btnClose.addEventListener('click', funRMV);
 		window.addEventListener('click', (event) => {
-			if (event.target === bgM) {
-				bgM.classList.remove('modal-visible');
-				bgM.innerHTML = '';
-				bgM.remove();
+			if (event.target === html.bgM) {
+				funRMV();
 			}
 		});
-		return {bgM};
+		return {winTemplate, title, btnMain};
 	}
-
-  function createModalConfirm() {
-    const winConfirm = document.createElement('div');
-    const btnClose = document.createElement('button');
-    const btnConfirm = document.createElement('button');
-    const btnCancel = document.createElement('button');
-    const title = document.createElement('h3');
+	
+	function createModalConfirm() {
     const descr = document.createElement('p');
-    const img = document.createElement('img');
-
-    // win.classList.add('win-confirm', 'win-active');
-    winConfirm.classList.add('win-confirm');
-    title.classList.add('modal-title');
-    btnClose.classList.add('modal-btnClose');
-    btnConfirm.classList.add('modal-btnConfirm', 'btn');
-    btnCancel.classList.add('modal-btnCancel');
+		const templateModal = createModalWindowTemplate();
+		
+		templateModal.winTemplate.classList.add('win-confirm');
     descr.classList.add('modal-descr');
-
-    title.textContent = 'Удалить клиента';
+		
+		templateModal.title.textContent = 'Удалить клиента';
     descr.textContent = 'Вы действительно хотите удалить данного клиента';
-    btnConfirm.textContent = 'Удалить';
-    btnCancel.textContent = 'Отмена';
-    img.src = 'img/close.svg';
+		templateModal.btnMain.textContent = 'Удалить';
+		
+		templateModal.title.after(descr);
 
-    btnClose.append(img);
-    winConfirm.append(title, btnClose, descr, btnConfirm, btnCancel);
-
-		const html = createBgModal();
-    html.bgM.classList.add('modal-visible');
-    html.bgM.append(winConfirm);
-    document.body.style.overflow = 'hidden';
-
-    function removeModalVisible() {
-      html.bgM.classList.remove('modal-visible');
-      html.bgM.innerHTML = '';
-      html.bgM.remove();
-      document.body.style.overflow = 'auto';
-    }
-
-    btnConfirm.addEventListener('click', removeModalVisible);
-    btnCancel.addEventListener('click', removeModalVisible);
-    btnClose.addEventListener('click', removeModalVisible);
-
-    return {btnConfirm}
+    return {btnMain: templateModal.btnMain}
   }
 
   document.addEventListener("DOMContentLoaded", () => {
-    const html = getHTMLElement();
-    let conf = createModalConfirm();
+    createModalConfirm();
 
-		conf.btnConfirm.addEventListener('click', () => {
-			console.log('сработало');
-		})
   });
 })();
