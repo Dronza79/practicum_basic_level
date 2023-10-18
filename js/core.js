3b0f6098e1661896f202641cc3b2995e37510446
Андрей Зайцев <mr_zaycev@mail.ru>
2023-10-18 16:20:50 +0300

    Replace core.js

--- js/core.js
+++ js/core.js
@@ -1,38 +1,44 @@
-export {parseFormData, getListClients};
+export {parseFormData, createBodyTable};
 
 async function parseFormData(form) {
-	let data = new FormData(form);
-	const person = {};
-	const contacts = [];
-	const contType = data.getAll('type');
-	const contVal = data.getAll('value');
-	
-	person.surname = data.get('surname');
-	person.name = data.get('name');
-	person.lastName = data.get('lastName');
-	for (let i = 0; i < contType.length; i++) {
-		const obj = {type: contType[i], value: contVal[i]};
-		contacts.push(obj);
-	}
-	person.contacts = contacts;
-	
-	console.log('person=', person);
-	// let json = JSON.stringify(person);
-	let response = await fetch('http://localhost:3000/api/clients', {
-		method: 'POST',
-		body: JSON.stringify(person),
-		headers: {'Content-Type': 'application/json; charset=utf-8'}
-	});
-	console.log(response.ok);
-	console.log(response.status);
-	let errors = await response.json()
-	// console.log(err);
-	return {ok: response.ok, errors};
+  let data = new FormData(form);
+  const person = {};
+  const contacts = [];
+  const contType = data.getAll('type');
+  const contVal = data.getAll('value');
+
+  person.surname = data.get('surname');
+  person.name = data.get('name');
+  person.lastName = data.get('lastName');
+  for (let i = 0; i < contType.length; i++) {
+    const obj = {type: contType[i], value: contVal[i]};
+    contacts.push(obj);
+  }
+  person.contacts = contacts;
+
+  console.log('person=', person);
+  // let json = JSON.stringify(person);
+  let response = await fetch('http://localhost:3000/api/clients', {
+    method: 'POST',
+    body: JSON.stringify(person),
+    headers: {'Content-Type': 'application/json; charset=utf-8'}
+  });
+  console.log(response.ok);
+  console.log(response.status);
+  let errors = await response.json()
+  // console.log(err);
+  return {ok: response.ok, errors};
 }
 
-async function getListClients() {
-	let response = await fetch('http://localhost:3000/api/clients');
-	let listClients = await response.json();
-	console.log(listClients);
-	console.log(Array.isArray(listClients));
+async function createBodyTable(htmlElement) {
+  let response = await fetch('http://localhost:3000/api/clients');
+  let listClients = await response.json();
+  let tableBody = document.createElement('table');
+  console.log(listClients);
+  console.log(Array.isArray(listClients));
+  const {generateClientData} = await import('./table.js');
+  for (const client of listClients) {
+    tableBody.append(generateClientData(client)); // Добавление строки с данными клиента
+  }
+  htmlElement.append(tableBody);
 }
