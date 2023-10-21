e358d41c395f3b41084129bb0f6c7d683bee4710
Андрей Зайцев <mr_zaycev@mail.ru>
2023-10-21 15:27:15 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@
     const addBtn = document.getElementById('add-btn');
     return {tHead, sch, addBtn};
   }
-  
+
+  // Функция обработки сортировки данных
   function addEventSortTableHead(listTableHead) {
     for (let colum of listTableHead) {
       const arrow = document.createElement('img');
@@ -18,9 +19,38 @@
       colum.append(arrow);
       if (colum.classList.contains('table_username')) colum.append(route);
       
-      colum.addEventListener('click', () => {
-        arrow.src = arrow.src ? 'img/arrow_up.svg': 'img/arrow_dwn.svg';
-        route.textContent = route.textContent ? 'Я-А': "А-Я";
+      colum.addEventListener('click', async () => {
+        if (!colum.dataset.sorted) {
+          listTableHead.map((el) => {
+            if (el.dataset.sorted) {
+              el.dataset.sorted = '';
+              el.querySelector('img').src = '';
+              if (el.querySelector('span')) el.querySelector('span').textContent = '';
+            }
+          });
+          colum.querySelector('img').src = 'img/arrow_dwn.svg';
+          if (colum.querySelector('span')) colum.querySelector('span').textContent = 'А-Я';
+          colum.dataset.sorted = 'dwn';
+          const {createBodyTable} = await import('./core.js');
+          await createBodyTable(`${colum.id}_${colum.dataset.sorted}`); // Формирование тела таблицы клиентов на основании списка
+        } else if (colum.dataset.sorted === 'dwn') {
+          colum.querySelector('img').src = 'img/arrow_up.svg';
+          if (colum.querySelector('span')) colum.querySelector('span').textContent = 'Я-А';
+          colum.dataset.sorted = 'up';
+          const {createBodyTable} = await import('./core.js');
+          await createBodyTable(`${colum.id}_${colum.dataset.sorted}`); // Формирование тела таблицы клиентов на основании списка
+        } else if (colum.dataset.sorted === 'up') {
+          colum.querySelector('img').src = 'img/arrow_dwn.svg';
+          if (colum.querySelector('span')) colum.querySelector('span').textContent = 'А-Я';
+          colum.dataset.sorted = 'dwn';
+          const {createBodyTable} = await import('./core.js');
+          await createBodyTable(`${colum.id}_${colum.dataset.sorted}`); // Формирование тела таблицы клиентов на основании списка
+        }
+        setTimeout(() => {
+          colum.dataset.sorted = '';
+          colum.querySelector('img').src = '';
+          if (colum.querySelector('span')) colum.querySelector('span').textContent = '';
+        }, 2000)
       });
     }
   }
